a2dbe528f6395df502507aca70be9b7e445f762d
CJ-Felicitas <cjbfelicitas@usep.edu.ph>
2024-02-26 16:02:07 +0800

    catch real ip address

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const app = express();
 app.set('trust proxy', true);  // Enable trusting of proxy headers
 
 app.get("/", (req, res) => {
-  const ipAddress = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+  const ipAddress = req.headers['x-real-ip'] || req.connection.remoteAddress;
   const ipv4Address = ipAddress.split(':').pop();  // Extract IPv4 address from ::ffff:127.0.0.1
 
   console.log(`Request from IP: ${ipv4Address}`);
