dab6a1263659eb69b8783a0c41721e262f0ee242
CJ-Felicitas <cjbfelicitas@usep.edu.ph>
2024-04-03 22:17:06 +0800

    refactored for controllers

--- routes/apiroutes.js
+++ routes/apiroutes.js
@@ -1,17 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const bodyParser = require('body-parser'); 
-const mysql = require('mysql');
-const axios = require('axios');
 
-// Create MySQL connection pool
-const pool = mysql.createPool({
-    connectionLimit: 10,
-    host: process.env.MYSQL_HOST,
-    user: process.env.MYSQL_USER,
-    password: process.env.MYSQL_PASSWORD,
-    database: process.env.MYSQL_DATABASE
-  });
+const {TestResponseController} = require('../controllers/TestResponseController');
+const {SubmitUrlController} = require('../controllers/SubmitUrlController');
+const {ReportUrlController} = require('../controllers/ReportUrlController');
 
 // Middleware to log IP addresses
 router.use((req, res, next) => {
@@ -20,95 +12,13 @@ router.use((req, res, next) => {
     next();
 });
 
+// test response if server is working
+router.get('/', TestResponseController);
 
-// test route
-router.get('/', (req, res) => {
-    console.log("Test response for GET route");
-    // Sending a JSON response
-    res.json({
-        message: 'Test response',
-        status: 'success'
-    });
-});
+// submit a url to the themis (flask) server.
+router.post('/submiturl', SubmitUrlController);
 
-// POST route
-router.post('/submiturl', async (req, res) => {
-    console.log("Received POST request to submiturl");
-    // Assuming you want to access data sent in the request body
-    const url = req.body.url;
-    try {
-        const response = await axios.post('http://127.0.0.1:5000/predict', { url });
-        console.log(response.data.received_data); // Log the response data
-        console.log(response.data.message);
-
-        const received_data = response.data.received_data;
-        
-        // Sending a JSON response
-        res.json({
-            flask: response.data.message,
-            message: 'I am from express server',
-            status: 'success',
-            data: received_data // Sending back the request data in response
-        });
-    } catch (error) {
-        console.error("Error making request to Flask:", error);
-        res.status(500).json({
-           
-            message: 'Error occurred while making request to Flask',
-            status: 'error'
-        });
-    }
-});
-// POST route to report URL
-router.post('/reportURL', (req, res) => {
-    console.log("reportURL");
-
-    const url = req.body.url;
-    
-    // Check if the URL exists in the table
-    pool.query('SELECT * FROM report_table WHERE url = ?', [url], (error, results, fields) => {
-        if (error) {
-            console.error("Error checking URL:", error);
-            res.status(500).json({
-                message: 'Error occurred',
-                status: 'error'
-            });
-        } else {
-            if (results.length > 0) {
-                // If URL exists, update the count
-                pool.query('UPDATE report_table SET report_count = report_count + 1 WHERE url = ?', [url], (error, results, fields) => {
-                    if (error) {
-                        console.error("Error incrementing count:", error);
-                        res.status(500).json({
-                            message: 'Error occurred',
-                            status: 'error'
-                        });
-                    } else {
-                        res.json({
-                            message: 'URL reported successfully',
-                            status: 'success'
-                        });
-                    }
-                });
-            } else {
-                // If URL doesn't exist, insert a new row
-                pool.query('INSERT INTO report_table (url, report_count) VALUES (?, 1)', [url], (error, results, fields) => {
-                    if (error) {
-                        console.error("Error inserting URL:", error);
-                        res.status(500).json({
-                            message: 'Error occurred',
-                            status: 'error'
-                        });
-                    } else {
-                        res.json({
-                            message: 'URL reported successfully',
-                            status: 'success'
-                        });
-                    }
-                });
-            }
-        }
-    });
-});
+// report a url then record it to the MySQL database server
+router.post('/reporturl', ReportUrlController);
 
 module.exports = router;
--- server.js
+++ server.js
@@ -3,7 +3,7 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
-const mysql = require('mysql');
+const mysql = require("mysql");
 
 // MongoDB Connection
 mongoose.connect(process.env.DATABASE_URL);
@@ -11,26 +11,23 @@ const db = mongoose.connection;
 db.on("error", (error) => console.error(error));
 db.once("open", () => console.log("Connected to MongoDB Atlas Database"));
 
-// MySQL Connection
 const pool = mysql.createPool({
   connectionLimit: 10,
   host: process.env.MYSQL_HOST,
   user: process.env.MYSQL_USER,
   password: process.env.MYSQL_PASSWORD,
-  database: process.env.MYSQL_DATABASE
+  database: process.env.MYSQL_DATABASE,
 });
 
 // Test the MySQL connection
 pool.getConnection((err, connection) => {
   if (err) {
-      console.error('Database connection failed: ' + err.stack);
-      return;
+    console.error("Database connection failed: " + err.stack);
+    return;
   }
-  console.log('Connected to MySQL database as ID ' + connection.threadId);
+  console.log("Connected to MySQL database as ID " + connection.threadId);
   connection.release(); // Release the connection
 });
-
-
 
 app.use(express.json());
 const apiRouter = require("./routes/apiroutes");
