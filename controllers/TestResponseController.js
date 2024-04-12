8be6ea45d172a2c9bb515ac283b5c486d12d0239
CJ-Felicitas <cjbfelicitas@usep.edu.ph>
2024-04-13 00:23:43 +0800

    7test

--- controllers/TestResponseController.js
+++ controllers/TestResponseController.js
@@ -2,7 +2,7 @@ exports.TestResponseController = (req, res) => {
     console.log("Test response for GET route");
     // Sending a JSON response
     res.json({
-        message: 'Test response from Express server #2 webhoosssssssk',
+        message: 'Test response from Express server #2 webhoosssssssk3333333',
         status: 'success'
     });
 };
