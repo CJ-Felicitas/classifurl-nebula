e664e485cb622bc33d8a9998721cd742215fd913
CJ-Felicitas <cjbfelicitas@usep.edu.ph>
2024-04-12 23:43:37 +0800

    test #3 webhook

--- controllers/TestResponseController.js
+++ controllers/TestResponseController.js
@@ -2,7 +2,7 @@ exports.TestResponseController = (req, res) => {
     console.log("Test response for GET route");
     // Sending a JSON response
     res.json({
-        message: 'Test response',
+        message: 'Test response from Express server',
         status: 'success'
     });
 };
