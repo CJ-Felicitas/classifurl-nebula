a77e4d5b7e12c8cf3b116c4a25ec34b30c9446ce
CJ-Felicitas <cjbfelicitas@usep.edu.ph>
2024-04-13 00:51:34 +0800

    last final update check

--- controllers/TestResponseController.js
+++ controllers/TestResponseController.js
@@ -2,7 +2,7 @@ exports.TestResponseController = (req, res) => {
     console.log("Test response for GET route");
     // Sending a JSON response
     res.json({
-        message: 'Test response from Nebula Server',
+        message: 'Test response from Nebula (Express) Server',
         status: 'success'
     });
 };
