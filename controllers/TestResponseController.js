2de7f8ec4dc1251efdd9a2fac120331234e75366
CJ-Felicitas <cjbfelicitas@usep.edu.ph>
2024-04-15 21:34:05 +0800

    package version added

--- controllers/TestResponseController.js
+++ controllers/TestResponseController.js
@@ -1,8 +1,12 @@
 exports.TestResponseController = (req, res) => {
     console.log("Test response for GET route");
+
     res.json({
         message: 'Test response from Nebula (Express) Server',
         status: 'success',
-        code: 200
+        code: 200,
+        package_version: '1.0.0',
     });
+
+    ;
 };
